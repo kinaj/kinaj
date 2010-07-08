1141b3da395d0e49e53321e5778d074a89ddcd8d
goldjunge <devnull@localhost>
2010-07-08 23:13:44 +0200

    removed model complexity, centralized for now

--- 3.0/models.js
+++ 3.0/models.js
@@ -1,6 +1,51 @@
 require('./models/user');
 
-var mongoose = require('mongoose/mongoose').Mongoose
-  , db = mongoose.connect('mongodb://127.0.0.1/kinaj');
+var sys = require('sys')
+  , crypto = require('crypto')
+  , helper = require('../helper')
+  , mongoose = require('mongoose/mongoose').Mongoose
+  , db = mongoose.connect('mongodb://127.0.0.1/kinaj')
+  , algo = 'sha512'
+  , hmacKey = 'com.kinaj.admin';
   
+function passwordHash(password, salt) {
+  var salt = salt || helper.fastUUID().substr(0, 6)
+    , hash = crypto.createHmac(algo, hmacKey).update([ salt, password ].join('')).digest('base64');
+
+  return [ algo, salt, hash ].join('$');
+};
+
+mongoose.model('User', {
+  properties: [ 'username', 'email', 'password', 'updated_at' ]
+, indexes: [ [{ 'username': 1}, {unique: true }] ]
+, setters: {
+    password: function(v) {
+      return passwordHash(v);
+    }
+  }
+, methods: {
+    passwordHash: passwordHash,
+    save: function(fn) {
+      this.updated_at = new Date();
+      this.__super__(fn);
+    }
+  }
+, static: {
+    passwordHash: passwordHash,
+    validPassword: function(username, password, cb) {
+      var self = this;
+
+      this.find({ username: username }).first(function(doc) {
+        if (doc) {
+          var salt = doc.password.split('$')[1]
+            , reference = self.passwordHash(password, salt)
+            , ret = (reference === doc.password);
+          
+          cb(ret, doc._id.toHexString());
+        } else return cb(false, null);
+      });
+    }
+  }
+});
+
 exports.User = db.model('User');
