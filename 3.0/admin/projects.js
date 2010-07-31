fbfa3efde6a629d80252b5aee8d35d0e83c8ded6
goldjunge <devnull@localhost>
2010-07-31 13:37:54 +0200

    added deletion for projects smaller cleanups

--- 3.0/admin/projects.js
+++ 3.0/admin/projects.js
@@ -2,18 +2,6 @@ var sys = require('sys')
   , Project = require('../models').Project
   , ins = function(x) { return sys.debug(sys.inspect(x, true, 5)); };
 
-exports.list = function(req, res, params) {
-  Project.find({}).all(function(projects) {
-    var projects = projects.map(function(p) { return p.__doc; })
-
-    ins(params);
-
-    if (params.xhr) {
-      res.template(200, {}, 'admin/partials/projects-list.html', { projects: projects });
-    } else res.template(200, {}, 'admin/projects-manage.html', { projects: projects });
-  });
-};
-
 exports.create = function(req, res, params) {
   project = new Project();
 
@@ -35,36 +23,66 @@ exports.update = function(req, res, params) {
 };
 
 exports.del = function(req, res, params) {
+  var msg = 'project successfully delete';
+
   Project.find({ slug: params.slug }).first(function(project) {
     project.remove(function() {
-      params.flash.push('project successfully delete', function() {
-        res.redirect('/projects');
-      });
+      if (params.xhr) {
+        res.simple(200, { msg: msg }, {});
+      } else {
+        params.flash.push(msg, function() {
+          res.redirect('/projects');
+        });
+      }
     });
   });
 };
 
+exports.list = function(req, res, params) {
+  Project.find({}).all(function(projects) {
+    var tmpl = 'admin/projects-manage.html'
+      , ctx = { projects: projects.map(function(p) { return p.__doc; }) };
+
+
+    if (params.xhr) {
+      tmpl = 'admin/partials/projects-list.html';
+    }
+    
+    res.template(200, {}, tmpl, ctx);
+  });
+};
+
 exports.newForm = function(req, res, params) {
-  var form = { submit: 'create'
-              , action: '/projects/create'
-              , method: 'post'
-              };
+  var tmpl = 'admin/projects-new.html'
+    , ctx = { project: {}
+            , _form: { submit: 'create'
+                     , action: '/projects/create'
+                     , method: 'post'
+                     }
+            };
   
   if (params.xhr) {
-    res.template(200, {}, 'admin/partials/projects-form.html', { _form: form });
-  } else res.template(200, {}, 'admin/projects-new.html', { _form: form });
+    tmpl = 'admin/partials/projects-form.html';
+  }    
+
+  res.template(200, {}, tmpl, ctx);
 };
 
 exports.editForm = function(req, res, params) {
-  Project.find({ 'slug': params.slug }).first(function(project) {
-    ctx = project.__doc;
-    ctx._form = { submit: 'update'
-                , action: '/projects/' + params.slug + '/update'
-                , method: 'post'
-                };
+  var tmpl = 'admin/projects-edit.html'
+    , ctx = {  _form: { submit: 'update'
+                     , action: '/projects/' + params.slug + '/update'
+                     , method: 'post'
+                     }
+            };
 
-    if (params.xhr) {
-      res.template(200, {}, 'admin/partials/projects-form.html', ctx);
-    } else res.template(200, {}, 'admin/projects-edit.html', ctx);
+  if (params.xhr) {
+    tmpl = 'admin/partials/projects-form.html';
+  }
+
+  Project.find({ 'slug': params.slug }).first(function(project) {
+    ctx.project = project;
+
+    res.template(200, {}, tmpl, ctx);
   });
 };
--- 3.0/static/js/kinaj.js
+++ 3.0/static/js/kinaj.js
@@ -1,10 +1,25 @@
 jQuery(function($) {
+  // simple ajaxified navigation
   $('#spaces').delegate('a', 'click', function(event) {
-    var link = $(this)
-      , workspace = $('#workspace');
+    var $link = $(this)
+      , $workspace = $('#workspace');
 
-    workspace.load(link.attr('href'));
-    console.log('click da shit');
+    $workspace.load($link.attr('href'));
+
+    event.preventDefault();
+  });
+
+  // delete over ajax
+  $('body').delegate('a.delete', 'click', function(event) {
+    var $link = $(this)
+      , $parent = $link.parent();
+
+    $.ajax({ url: $link.attr('href')
+           , cache: false
+           , dataType: 'json'
+           , type: 'delete'
+           , success: function(res) { $parent.fadeOut(200); }
+           });
 
     event.preventDefault();
   });
