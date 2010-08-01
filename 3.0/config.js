58b288bf4b577acc6046616d754280be633534af
goldjunge <devnull@localhost>
2010-08-01 20:45:15 +0200

    removed appDomain and added tmpDir to config

--- 3.0/config.js
+++ 3.0/config.js
@@ -4,5 +4,5 @@ var path = require('path')
 exports.runtime = runtime;
 exports.baseDir = __dirname;
 exports.viewDir = path.join(__dirname, 'views');
-exports.appDomain = (runtime === 'development') ? 'kinaj.dev' : 'kinaj.com';
+exports.tmpDir = path.join(__dirname, 'tmp');
 exports.domain = (runtime === 'development') ? 'kinaj.dev' : 'kinaj.com';
