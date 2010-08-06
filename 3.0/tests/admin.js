3c737fdca402aeb4b23b8398b803f25b2aefec16
goldjunge <a.simmerl@googlemail.com>
2010-08-06 20:08:38 +0200

    small cleanup

--- 3.0/tests/admin.js
+++
@@ -1,7 +0,0 @@
-var sys = require('sys')
-  , assert = require('assert')
-  , admin = { auth: require('../admin/auth'), projects: require('../admin/projects') };
-
-assert.ok(false, 'works');
-
-process.exit();
