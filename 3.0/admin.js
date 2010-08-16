5e851f0bd1a267d08366c3673a8a48d8451f0b06
goldjunge <devnull@localhost>
2010-08-16 12:48:54 +0200

    changed initiation to app instead of extending the original Ordnung object

--- 3.0/admin.js
+++ 3.0/admin.js
@@ -1,27 +1,25 @@
-var sys = require('sys')
-  , Ordnung = require('ordnung').Ordnung
-  , middleware = require('./middleware')
-  , mixins = require('./mixins')
-  , config = require('./config')
-  , base = require('./admin/base')
-  , auth = require('./admin/auth')
-  , projects = require('./admin/projects')
+var Ordnung     = require('ordnung').Ordnung
+  , middleware  = require('./middleware')
+  , mixins      = require('./mixins')
+  , config      = require('./config')
+  , base        = require('./admin/base')
+  , auth        = require('./admin/auth')
+  , projects    = require('./admin/projects')
   , attachments = require('./admin/attachments');
 
-Ordnung.prototype.middleware = [ middleware.logger
-                               , middleware.responseTime
-                               , middleware.xhr
-                               , middleware.form
-                               , middleware.cookies
-                               , middleware.session
-                               , middleware.flash
-                               , middleware.authorization
-                               ];
-
-Ordnung.prototype.response.mixin({ template: mixins.template });
 
 var app = new Ordnung({ name: 'kinaj/admin', port: 3001 });
 
+app.middleware = [ middleware.logger
+                 , middleware.responseTime
+                 , middleware.xhr
+                 , middleware.form
+                 , middleware.cookies
+                 , middleware.session
+                 , middleware.flash
+                 , middleware.authorization
+                 ];
+app.res.mixin({ template: mixins.template });
 app.mapRoutes([
               // base route
                 [ [ 'get' ],    '/',        base.dashboard, true ]
@@ -43,4 +41,4 @@ app.mapRoutes([
               , [ [ 'get' ],    '/projects/:slug/edit',   projects.editForm, true ]
               ]);
 
-app.start();
+app.start()
