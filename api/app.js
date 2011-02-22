04e2c5d25aa1939c6e75ffa7c83ee962b0b02441
Alexander Simmerl <alx@soundcloud.com>
2011-02-22 22:37:57 +0100

    cleanup in app skeleton

--- api/app.js
+++ api/app.js
@@ -1,47 +1,38 @@
+var express = require('express')
+  , app = module.exports = express.createServer();
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyDecoder());
-  app.use(express.methodOverride());
-  app.use(express.cookieDecoder());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-  app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
-});
-
+// configuration
+app.configure(function() {
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade')
+  app.use(express.bodyDecoder())
+  app.use(express.methodOverride())
+  app.use(express.cookieDecoder())
+  app.use(express.session({ secret: 'your secret here' }))
+  app.use(express.compiler({ src: __dirname + '/public', enable: [ 'less' ] }))
+  app.use(app.router)
+  app.use(express.staticProvider(__dirname + '/public'))
+})
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+})
+app.configure('test', function() {})
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+  app.use(express.errorHandler())
+})
 
-// Routes
-
+// routes
 app.get('/', function(req, res){
   res.render('index', {
     locals: {
       title: 'Express'
     }
-  });
-});
+  })
+})
 
 // Only listen on $ node app.js
-
-if (!module.parent) {
-  app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port)
+if(!module.parent) {
+  app.listen(3000, function() {
+    console.log("Express server listening on port %d", app.address().port)
+  })
 }
