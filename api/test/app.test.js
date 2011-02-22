ed302b28231c8f18e8cb1f8ac272c66c24046317
Alexander Simmerl <alx@soundcloud.com>
2011-02-22 22:37:35 +0100

    test structure for api testing

--- api/test/app.test.js
+++ api/test/app.test.js
@@ -1,21 +1,63 @@
-
-// Run $ expresso
-
-/**
- * Module dependencies.
- */
+// correct env variable for tests
+process.env.NODE_ENV = 'test'
 
 var app = require('../app')
-  , assert = require('assert');
+  , assert = require('assert')
 
+// deliver html
+exports['GET /'] = function(beforeExit){
+  assert.response(app,
+    { url: '/' },
+    { status: 200,
+      headers: { 'content-type': 'text/html; charset=utf-8' }
+    }, function(res){
+      assert.includes(res.body, '<title>Express</title>')
+    })
+}
 
-module.exports = {
-  'GET /': function(){
-    assert.response(app,
-      { url: '/' },
-      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
-      function(res){
-        assert.includes(res.body, '<title>Express</title>');
-      });
-  }
-};
+// projects
+exports['GET /projects'] = function() {
+  assert.response(app, {
+    method: 'get',
+    url: '/projects',
+    headers: { 'accept': 'application/json' }
+  }, {
+    status: 200,
+    headers: { 'content-type': 'application/json' }
+  })
+}
+
+exports['POST /projects'] = function() {
+  assert.response(app, {
+    method: 'post',
+    url: '/projects',
+    headers: { 'accept': 'application/json' },
+    data: JSON.stringify({ title: 'foo', slug: 'foo', description: 'foo bar baz' })
+  }, {
+    status: 200,
+    headers: { 'content-type': 'application/json' }
+  })
+}
+
+exports['PUT /projects/foo'] = function() {
+  assert.response(app, {
+    method: 'put',
+    url: '/projects/foo',
+    headers: { 'accept': 'application/json' },
+    data: JSON.stringify({ description: 'foo bar barz' })
+  }, {
+    status: 200,
+    headers: { 'content-type': 'application/json' }
+  })
+}
+
+exports['DELETE /projects/foo'] = function() {
+  assert.response(app, {
+    method: 'delete',
+    url: '/projects/foo',
+    headers: { 'accept': 'application/json' },
+  }, {
+    status: 200,
+    headers: { 'content-type': 'application/json' }
+  })
+}
