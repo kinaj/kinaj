4a0b10622dac09a3df5813fcc58fd2090f16aa86
Alexander Simmerl <alx@soundcloud.com>
2011-02-23 16:55:13 +0100

    project tests in vows

--- api/test/app.test.js
+++ api/test/app.test.js
@@ -32,84 +32,68 @@ step(function() {
         assert.strictEqual(res.headers['content-type'], 'application/json')
       },
       'should respond with list of projects': function(err, res) {
-        assert.length(JSON.parse(res.body), 3)
+        assert.isArray(JSON.parse(res.body))
+        assert.notEqual(JSON.parse(res.body).length, 0)
+      }
+    },
+    'POST /projects': {
+      topic: function() {
+        var body = JSON.stringify({ title: 'foo', slug: 'foo', description: 'foo bar baz', tags: [ 'one', 'two', 'three' ] })
+        
+        api.request({
+          method: 'post',
+          path: '/projects',
+          headers: { 'content-type': 'application/json' },
+          data: body
+        }, this.callback)
+      },
+      'should respond with 200': function(err, res) {
+        assert.equal(res.statusCode, 200)
+      },
+      'should respond with correct content-type': function(err, res) {
+        assert.strictEqual(res.headers['content-type'], 'application/json')
+      },
+      'should respond with created document': function(err, res) {
+        assert.include(res.body, '"title":"foo"')
+      },
+    },
+    'PUT /projects/foo': {
+      topic: function() {
+        var body = JSON.stringify({ description: 'foo bar barz' })
+       
+        api.request({
+          method: 'put',
+          path: '/projects/foo',
+          headers: { 'content-type': 'application/json', 'content-length': body.length },
+          data: body
+        }, this.callback)
+      },
+      'should respond with 200': function(err, res) {
+        assert.equal(res.statusCode, 200)
+      },
+      'should respond with correct content-type': function(err, res) {
+        assert.strictEqual(res.headers['content-type'], 'application/json')
+      },
+      'should respond with updated document': function(err, res) {
+        assert.includes(res.body, '"description":"foo bar barz"')
+      }
+    },
+    'DELETE /projects/foo': {
+      topic: function() {
+        api.request({
+          method: 'delete',
+          path: '/projects/foo',
+        }, this.callback)
+      },
+      'should respond with 200': function(err, res) {
+        assert.equal(res.statusCode, 200)
+      },
+      'should respond with correct content-type': function(err, res) {
+        assert.strictEqual(res.headers['content-type'], 'application/json')
+      },
+      'should respond with updated document': function(err, res) {
+        assert.deepEqual(JSON.parse(res.body), { ok: true })
       }
     }
   }).run({ reporter: require('vows/lib/vows/reporters/spec') })
 })
-
-
-// exports['cleanup'] = function() {
-// }
-// exports['fixtures'] = function() {
-// }
-// 
-// // deliver html
-// exports['GET /'] = function(){
-//   assert.response(app,
-//     { url: '/' },
-//     { status: 200,
-//       headers: { 'content-type': 'text/html; charset=utf-8' }
-//     }, function(res){
-//       assert.includes(res.body, '<title>Express</title>')
-//     })
-// }
-// 
-// // projects
-// exports['GET /projects'] = function() {
-//   assert.response(app, {
-//     method: 'get',
-//     url: '/projects',
-//     headers: { 'accept': 'application/json' }
-//   }, {
-//     status: 200,
-//     headers: { 'content-type': 'application/json' }
-//   }, function(res) {
-//     assert.length(JSON.parse(res.body), 3, 'Should return list of projects')
-//   })
-// }
-// 
-// exports['POST /projects'] = function() {
-//   var body = JSON.stringify({ title: 'foo', slug: 'foo', description: 'foo bar baz', tags: [ 'one', 'two', 'three' ] })
-// 
-//   assert.response(app, {
-//     method: 'post',
-//     url: '/projects',
-//     headers: { 'accept': 'application/json', 'content-type': 'application/json', 'content-length': body.length },
-//     data: body
-//   }, {
-//     status: 200,
-//     headers: { 'content-type': 'application/json' }
-//   }, function(res) {
-//     assert.includes(res.body, '"title":"foo"', 'Should return created project')
-//   })
-// }
-// 
-// exports['PUT /projects/foo'] = function() {
-//   var body = JSON.stringify({ description: 'foo bar barz' })
-// 
-//   assert.response(app, {
-//     method: 'put',
-//     url: '/projects/foo',
-//     headers: { 'accept': 'application/json', 'content-type': 'application/json', 'content-length': body.length },
-//     data: body
-//   }, {
-//     status: 200,
-//     headers: { 'content-type': 'application/json' }
-//   }, function(res) {
-//     assert.includes(res.body, '"description":"foo bar barz"', 'Should return the updated project')
-//   })
-// }
-// 
-// exports['DELETE /projects/foo'] = function(beforeExit) {
-//   assert.response(app, {
-//     method: 'delete',
-//     url: '/projects/foo',
-//     headers: { 'accept': 'application/json' },
-//   }, {
-//     status: 200,
-//     headers: { 'content-type': 'application/json' }
-//   }, function(res) {
-//     assert.eql(JSON.parse(res.body), { ok: true }, 'Should acknowledge the deletion')
-//   })
-// }
