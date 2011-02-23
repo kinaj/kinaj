209e19fb71e7e6456f9e63afdd8bae92721e583f
Alexander Simmerl <alx@soundcloud.com>
2011-02-23 14:58:49 +0100

    new test approach with vows

--- api/test/app.test.js
+++ api/test/app.test.js
@@ -1,83 +1,116 @@
-// correct env variable for tests
-process.env.NODE_ENV = 'test'
 
-var assert = require('assert')
+var http = require('http')
+  , assert = require('assert')
+  , step = require('step')
+  , vows = require('vows')
   , app = require('../app')
   , helpers = require('./helpers')
 
-exports['cleanup'] = function() {
-  helpers.dropDatabase(app.set('mongodb'))
-}
-exports['fixtures'] = function() {
-  helpers.applyFixtures(app)
-}
+// preparations
+process.env.NODE_ENV = 'test' // correct env variable for tests
 
-// deliver html
-exports['GET /'] = function(){
-  assert.response(app,
-    { url: '/' },
-    { status: 200,
-      headers: { 'content-type': 'text/html; charset=utf-8' }
-    }, function(res){
-      assert.includes(res.body, '<title>Express</title>')
-    })
-}
+step(function dropDatabase() {
+  helpers.dropDatabase(app.set('mongodb'), this)
+}, function applyFixtures() {
+  helpers.applyFixtures(app, this)
+}, function runTests() {
+  // tests
+  vows.describe('kinaj').addBatch({
+    'GET /': {
+      topic: function() {
+        var that = this
+          , options = { host: 'localhost'
+                      , port: 3000
+                      , path: '/'
+                      , method: 'get'
+                      }
 
-// projects
-exports['GET /projects'] = function() {
-  assert.response(app, {
-    method: 'get',
-    url: '/projects',
-    headers: { 'accept': 'application/json' }
-  }, {
-    status: 200,
-    headers: { 'content-type': 'application/json' }
-  }, function(res) {
-    assert.length(JSON.parse(res.body), 3, 'Should return list of projects')
-  })
-}
+        http.request(options, function(res) {
+          that.callback(null, res)
+        }).end()
+      },
+      'should respond with statusCode 200': function(err, res) {
+        assert.equal(res.statusCode, 200)
+      },
+      'should respond with header content-type': function(err, res) {
+        assert.strictEqual(res.headers['content-type'], 'text/html; charset=utf-8')
+      }
+    }
+  }).run()
+})
 
-exports['POST /projects'] = function() {
-  var body = JSON.stringify({ title: 'foo', slug: 'foo', description: 'foo bar baz', tags: [ 'one', 'two', 'three' ] })
 
-  assert.response(app, {
-    method: 'post',
-    url: '/projects',
-    headers: { 'accept': 'application/json', 'content-type': 'application/json', 'content-length': body.length },
-    data: body
-  }, {
-    status: 200,
-    headers: { 'content-type': 'application/json' }
-  }, function(res) {
-    assert.includes(res.body, '"title":"foo"', 'Should return created project')
-  })
-}
-
-exports['PUT /projects/foo'] = function() {
-  var body = JSON.stringify({ description: 'foo bar barz' })
-
-  assert.response(app, {
-    method: 'put',
-    url: '/projects/foo',
-    headers: { 'accept': 'application/json', 'content-type': 'application/json', 'content-length': body.length },
-    data: body
-  }, {
-    status: 200,
-    headers: { 'content-type': 'application/json' }
-  }, function(res) {
-    assert.includes(res.body, '"description":"foo bar barz"', 'Should return the updated project')
-  })
-}
-
-exports['DELETE /projects/foo'] = function(beforeExit) {
-  assert.response(app, {
-    method: 'delete',
-    url: '/projects/foo',
-    headers: { 'accept': 'application/json' },
-  }, {
-    status: 200,
-    headers: { 'content-type': 'application/json' }
-  }, function(res) {
-    assert.eql(JSON.parse(res.body), { ok: true }, 'Should acknowledge the deletion')
-  })
-}
+// exports['cleanup'] = function() {
+// }
+// exports['fixtures'] = function() {
+// }
+// 
+// // deliver html
+// exports['GET /'] = function(){
+//   assert.response(app,
+//     { url: '/' },
+//     { status: 200,
+//       headers: { 'content-type': 'text/html; charset=utf-8' }
+//     }, function(res){
+//       assert.includes(res.body, '<title>Express</title>')
+//     })
+// }
+// 
+// // projects
+// exports['GET /projects'] = function() {
+//   assert.response(app, {
+//     method: 'get',
+//     url: '/projects',
+//     headers: { 'accept': 'application/json' }
+//   }, {
+//     status: 200,
+//     headers: { 'content-type': 'application/json' }
+//   }, function(res) {
+//     assert.length(JSON.parse(res.body), 3, 'Should return list of projects')
+//   })
+// }
+// 
+// exports['POST /projects'] = function() {
+//   var body = JSON.stringify({ title: 'foo', slug: 'foo', description: 'foo bar baz', tags: [ 'one', 'two', 'three' ] })
+// 
+//   assert.response(app, {
+//     method: 'post',
+//     url: '/projects',
+//     headers: { 'accept': 'application/json', 'content-type': 'application/json', 'content-length': body.length },
+//     data: body
+//   }, {
+//     status: 200,
+//     headers: { 'content-type': 'application/json' }
+//   }, function(res) {
+//     assert.includes(res.body, '"title":"foo"', 'Should return created project')
+//   })
+// }
+// 
+// exports['PUT /projects/foo'] = function() {
+//   var body = JSON.stringify({ description: 'foo bar barz' })
+// 
+//   assert.response(app, {
+//     method: 'put',
+//     url: '/projects/foo',
+//     headers: { 'accept': 'application/json', 'content-type': 'application/json', 'content-length': body.length },
+//     data: body
+//   }, {
+//     status: 200,
+//     headers: { 'content-type': 'application/json' }
+//   }, function(res) {
+//     assert.includes(res.body, '"description":"foo bar barz"', 'Should return the updated project')
+//   })
+// }
+// 
+// exports['DELETE /projects/foo'] = function(beforeExit) {
+//   assert.response(app, {
+//     method: 'delete',
+//     url: '/projects/foo',
+//     headers: { 'accept': 'application/json' },
+//   }, {
+//     status: 200,
+//     headers: { 'content-type': 'application/json' }
+//   }, function(res) {
+//     assert.eql(JSON.parse(res.body), { ok: true }, 'Should acknowledge the deletion')
+//   })
+// }
--- api/test/helpers.js
+++ api/test/helpers.js
@@ -1,7 +1,8 @@
 var url = require('url')
+  , assert = require('assert')
   , mongodb = require('mongodb')
 
-exports.dropDatabase = function(dbpath) {
+exports.dropDatabase = function(dbpath, callback) {
   var config = url.parse(dbpath)
     , db = new mongodb.Db(config.pathname.replace('/', ''), new mongodb.Server(config.hostname, config.port, {}), { native_parser: true })
 
@@ -10,19 +11,29 @@ exports.dropDatabase = function(dbpath) {
 
     db.dropDatabase(function(err, res) {
       if(err) throw err
+
+      db.close()
+
+      callback()
     })
   })
 }
-exports.applyFixtures = function(app) {
+exports.applyFixtures = function(app, callback) {
   var projects = [ 
     { title: 'first', slug: 'first', description: 'first description' },
     { title: 'second', slug: 'second', description: 'second description' },
     { title: 'third', slug: 'third', description: 'third description' }
-  ]
+  ], done = 0
 
   projects.forEach(function(p, i) {
     new app.Project(p).save(function(err) {
       if(err) throw err
+
+      done++
+
+      if(done === projects.length) {
+        callback()
+      }
     })
   })
 }
